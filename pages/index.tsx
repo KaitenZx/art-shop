492e1b6734b9ed9b1c633f7f3ea41199e97de5fd
Yura Glonti <kaiten@Georges-MacBook-Pro.local>
2024-07-22 02:16:58 +0400

    4prt

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import Link from "next/link";
 import Layout from "@/components/layout";
 
 const inter = Inter({ subsets: ["latin"] });
