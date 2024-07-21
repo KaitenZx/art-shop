fa53b2199ba5aed12d4cb65f19501fd89ceeaa07
Yura Glonti <kaiten@Georges-MacBook-Pro.local>
2024-07-22 02:37:53 +0400

    4prt

--- pages/gallery/[id].tsx
+++ pages/gallery/[id].tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import styles from './[id].module.css';
 import { useState } from 'react';
-import Modal from '@/components/modal';
+import Modal from '@/components/Modal';
 
 interface ArtDetails {
 	name: string;
